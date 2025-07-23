f66631cd654564a71528ea13d14e013d1ffce738
imbilgates <bilgates270703@gmail.com>
2025-07-24 02:03:43 +0530

    env changed

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,28 +1,27 @@
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwd8r3qWC6hbwYkeUfPpbLutQazSxIBsM",
-  authDomain: "chatwithme-f4d3b.firebaseapp.com",
-  databaseURL: "https://chatwithme-f4d3b-default-rtdb.firebaseio.com",
-  projectId: "chatwithme-f4d3b",
-  storageBucket: "chatwithme-f4d3b.appspot.com",
-  messagingSenderId: "1063708558827",
-  appId: "1:1063708558827:web:11def669906227e329c5ae"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-// Use this for React Native apps to ensure persistence across sessions
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-export { auth };
